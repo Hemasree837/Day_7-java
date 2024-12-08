00443be028edbd7bd3e86975d7bf9366f1e3d331
Hemasree837 <hemasree837@gmail.com>
2024-12-08 15:06:28 +0530

    Add files via upload

---
+++ MovingZerosD.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class MovingZerosD{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in); 
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int temp[]=new int[n];
+        int p=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            if(arr[i]!=0)
+            {
+                temp[p]=arr[i];
+                p++;
+            }
+        } 
+    System.arraycopy(temp,0,arr,0,n);
+    System.out.println(Arrays.toString(arr));
+}
+}
+/*
+ * 7
+1
+0
+0
+12
+0
+22
+11
+[1, 12, 22, 11, 0, 0, 0]
+ */
