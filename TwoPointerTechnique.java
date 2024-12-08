978137b7ee958021a61dab80a278598395664cbd
Hemasree837 <hemasree837@gmail.com>
2024-12-08 11:36:18 +0530

    Add files via upload

---
+++ TwoPointerTechnique.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class TwoPointerTechnique
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        arr[i]=sc.nextInt();
+        int l=0;
+        int h=n-1;
+        while(l<=h)
+        {
+            int temp=arr[l];
+            arr[l]=arr[h];
+            arr[h]=temp;
+            l++;
+            h--;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
